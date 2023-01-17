06bdecbd23065c1e99e4da9a9ced671e591a43b3
rabobahago <rabobahago@gmail.com>
2023-01-17 05:23:02 +0100

    helper hardhat config variables added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,50 +1,56 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("dotenv").config();
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
-  solidity: "0.8.7",
-  //defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
+    solidity: {
+        compilers: [
+            { version: "0.8.7" },
+            { version: "0.6.6" },
+            { version: "0.4.19" },
+        ],
     },
-    goerli: {
-      chainId: 5,
-      blockComfirmations: 6,
-      url: GOERLI_RPC_URL,
-      accounts: [PRIVATE_KEY],
+    //defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        goerli: {
+            chainId: 5,
+            blockComfirmations: 6,
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
     },
-  },
-  gasReporter: {
-    enabled: false,
-    outputFile: "gas-reporter.txt",
-    noColors: true,
-    currency: "USD",
-    coinmarketcap: COINMARKETCAP_API_KEY,
-    token: "MATIC",
-  },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-reporter.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
     },
-    player: {
-      default: 1,
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
-  },
-  mocha: {
-    timeout: 200000,
-  },
-};
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 200000,
+    },
+}
