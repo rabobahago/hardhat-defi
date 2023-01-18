747a7f99369e4b52e631d4bef1a5abebe7075cca
rabobahago <rabobahago@gmail.com>
2023-01-18 02:40:57 +0100

    aave borrow, get weth and Aggregator v2 interface added

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -1,7 +1,21 @@
-const { getNamedAccounts } = require("hardhat")
+const { ethers, getNamedAccounts, network } = require("hardhat")
+const { networkConfig } = require("../helper-hardhat-config")
+
+const AMOUNT = ethers.utils.parseEther("0.1")
+
 async function getWeth() {
     const { deployer } = await getNamedAccounts()
-    //call the deposit on the weth contract
-    //abi, contracts
+    const iWeth = await ethers.getContractAt(
+        "IWeth",
+        networkConfig[network.config.chainId].wethToken,
+        deployer
+    )
+    const txResponse = await iWeth.deposit({
+        value: AMOUNT,
+    })
+    await txResponse.wait(1)
+    const wethBalance = await iWeth.balanceOf(deployer)
+    console.log(`Got ${wethBalance.toString()} WETH`)
 }
-module.exports = { getWeth }
+
+module.exports = { getWeth, AMOUNT }
