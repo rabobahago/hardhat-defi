c543da4e6106d7afe4712046e56ab2a0f9e39f22
rabobahago <rabobahago@gmail.com>
2023-01-18 02:20:19 +0100

    IERC20 and ILendingPool solidity files added

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,7 @@ module.exports = {
         compilers: [
             { version: "0.8.7" },
             { version: "0.6.6" },
+            { version: "0.6.12" },
             { version: "0.4.19" },
         ],
     },
